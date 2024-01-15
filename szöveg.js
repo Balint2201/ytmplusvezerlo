1172804b18268d7e83d622ab816b9e11dde20eed
Balint2201 <116822005+Balint2201@users.noreply.github.com>
2024-01-15 19:36:45 +0100

    Add files via upload

---
+++ szöveg.js
@@ -0,0 +1,6 @@
+        <a href="https://bit.ly/HMNetStat" target="_blank">Internet kapcsolat ellenörzése</a>
+        <a href="https://bit.ly/HMKeparany" target="_blank">Képarány</a>
+        <a href="https://bit.ly/HMKarakterSzam" target="_blank">Karakter számláló</a>
+        <a href="https://bit.ly/HMLepegeto" target="_blank">Lépegető</a>
+        <a href="https://coinflip.horvathmartonxd.repl.co" target="_blank">Coin Flip</a>
+        <a href="https://bit.ly/HMBlobKeszito" target="_blank">Blob Készítő</a>
